5b9c962c9abfea1d0af42363cb122a172a5b3c55
manturovDan <manturov.dan@mail.ru>
2020-03-31 23:50:51 +0300

    before un min

--- src/man/dan/converter/unittest/ParserTest.java
+++ src/man/dan/converter/unittest/ParserTest.java
@@ -1,7 +1,15 @@
 package man.dan.converter.unittest;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParserTest {
+    @Test
+    public void testJUnit() {
+        Assert.assertEquals(1, 1);
+    }
+
 
 }
