611e2cbcb62a87796d722d8e8c87a15b98793555
manturovDan <manturov.dan@mail.ru>
2020-04-01 16:53:23 +0300

    merging is ready

--- src/ConverterLaunch.java
+++ src/ConverterLaunch.java
@@ -2,11 +2,17 @@ import man.dan.converter.lexer.Lexer;
 import man.dan.converter.parser.Parser;
 import man.dan.converter.parser.SyntaxError;
 import man.dan.converter.parser.TypeError;
+import man.dan.converter.representation.Call;
+import man.dan.converter.transformer.Merger;
 
-public class ConverterLaunch {
-    public static void main(String[] args) throws SyntaxError, TypeError {
+import java.util.LinkedList;
+
+public class ConverterLaunch  {
+    public static void main(String[] args) throws Exception {
         Lexer lex = new Lexer(args[0]);
         Parser parser = new Parser(lex);
-        parser.analysis();
+        LinkedList<Call> callChain = parser.analysis();
+        Merger merger = new Merger(callChain);
+        merger.transform();
     }
 }
---
+++ src/man/dan/converter/transformer/Merger.java
@@ -0,0 +1,72 @@
+package man.dan.converter.transformer;
+
+import man.dan.converter.representation.Call;
+import man.dan.converter.representation.FilterCall;
+import man.dan.converter.representation.MapCall;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class Merger {
+    LinkedList<Call> chain;
+
+    public Merger(LinkedList<Call> ch) {
+        chain = ch;
+    }
+
+    public void transform() throws Exception {
+        ListIterator<Call> iter = chain.listIterator();
+
+        //check empty list
+
+        Call previous;
+        Call current = iter.next();
+        while (iter.hasNext()) {
+
+            System.out.println(chain);
+
+            previous = current;
+            current = iter.next();
+
+            if (previous instanceof FilterCall && current instanceof FilterCall) {
+                mergeFF((FilterCall) previous, (FilterCall) current);
+            }
+            else if (previous instanceof MapCall && current instanceof MapCall) {
+                mergeMM((MapCall)previous, (MapCall) current);
+            }
+            else if (previous instanceof MapCall && current instanceof FilterCall) {
+                mergeMF((MapCall)previous, (FilterCall) current);
+            }
+            else if (previous instanceof FilterCall && current instanceof MapCall) {
+                continue;
+            }
+            else
+                throw new Exception("Something bad");
+
+            iter.previous();
+            iter.previous();
+            iter.remove();
+            if (iter.hasPrevious())
+                iter.previous();
+
+            if (iter.hasPrevious())
+                iter.previous();
+            current = iter.next();
+        }
+
+
+        System.out.println(chain);
+    }
+
+    protected void mergeFF(FilterCall prev, FilterCall cur) {
+        //
+    }
+
+    protected void mergeMM(MapCall prev, MapCall cur) {
+        //
+    }
+
+    protected void mergeMF(MapCall prev, FilterCall cur) {
+        //
+    }
+}
