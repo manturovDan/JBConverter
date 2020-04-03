2190709d2985439ec087f0d1fe7c0b6089c21663
manturovDan <manturov.dan@mail.ru>
2020-04-03 21:18:43 +0300

    bug was found

--- src/man/dan/converter/unittest/MergerTest.java
+++ src/man/dan/converter/unittest/MergerTest.java
@@ -27,12 +27,33 @@ class MergerTest {
     @Test
     public void example1Test() throws Exception {
         Assert.assertEquals(allSteps("filter{(element>10)}%>%filter{(element>10)}"),
-                "filter{(element>10)&(element>10)}%>%map{element}");
+                "filter{((element>10)&(element>10))}%>%map{element}");
     }
 
     @Test
     public void example2Test() throws Exception {
         Assert.assertEquals(allSteps("map{(element+10)}%>%filter{(element>10)}%>%map{(element*element)}"),
                 "filter{((element+10)>10)}%>%map{((element+10)*(element+10))}");
+    }
+
+    @Test
+    public void example3Test() throws Exception {
+        Assert.assertEquals(allSteps("filter{(element>10)}%>%filter{(element<10)}%>%map{(element*element)}"),
+                "filter{((element>10)&(element<10))}%>%map{(element*element)}");
+    }
+
+    @Test
+    public void exampleMy1() throws Exception {
+        Assert.assertEquals(allSteps("filter{((element+(15*3))=(((element+4)*10)-5))}%>%filter{(((5+3)<8)|(1=0))}%>%filter{((((5+3)<8)|(1=0))&(element>-9))}"),
+                "filter{((((element+(15*3))=(((element+4)*10)-5))&(((5+3)<8)|(1=0)))&((((5+3)<8)|(1=0))&(element>-9)))}%>%map{element}");
+
+        Assert.assertEquals(allSteps("filter{((element+(15*3))=(((element+4)*10)-5))}%>%filter{(((5+3)<8)|(1=0))}%>%filter{((((5+3)<8)|(1=0))&(element>-9))}%>%map{(element+1)}"),
+                "filter{((((element+(15*3))=(((element+4)*10)-5))&(((5+3)<8)|(1=0)))&((((5+3)<8)|(1=0))&(element>-9)))}%>%map{(element+1)}");
+    }
+
+    @Test
+    public void exampleMy2() throws Exception {
+        Assert.assertEquals(allSteps("map{((((10000*element)*20)+15)*0)}%>%map{(((element+188)*38)-2)}%>%map{(3*1)}%>%map{element}"),
+                "filter{(1=1)}%>%map{(3*1)}");
     }
 }
